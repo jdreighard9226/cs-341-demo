c2305a9c50f97acb1bb78aea9bd79b34b1d26e5d
JD Reighard <jreighard9226@gmail.com>
2026-08-28 11:13:10 -0600

    added validation checking for login functionality

--- src/main/java/edu/carroll/cs341/web/controller/LoginController.java
+++ src/main/java/edu/carroll/cs341/web/controller/LoginController.java
@@ -1,14 +1,21 @@
 package edu.carroll.cs341.web.controller;
 
 import edu.carroll.cs341.web.form.LoginForm;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
+    // XXX - If anything like this is used in a real application, I will hunt you down and embarrass you to all your peers.
+    private static final String validUser = "cs341user";
+    private static final String validPass = "supersecret";
     @GetMapping("/login")
     public String loginGet(Model model) {
         model.addAttribute("loginForm", new LoginForm());
@@ -16,13 +23,25 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String loginPost(@ModelAttribute LoginForm loginForm) {
-        System.out.println("User '" + loginForm.getUsername() + "' attempted login");
+    public String loginPost(@Valid @ModelAttribute LoginForm loginForm, BindingResult result, RedirectAttributes attrs) {
+        if (result.hasErrors()) {
+            return "login";
+        }
+
+        // Username does not have to match the case, but password must be an exact match.
+        // XXX - NEVER do password validation using a hardcoded string
+        if (!(validUser.equalsIgnoreCase(loginForm.getUsername()) &&
+                validPass.equals(loginForm.getPassword()))) {
+            result.addError(new ObjectError("globalError", "Username and password do not match known users"));
+            return "login";
+        }
+        attrs.addAttribute("username", loginForm.getUsername());
         return "redirect:/loginSuccess";
     }
 
     @GetMapping("/loginSuccess")
-    public String loginSuccess() {
+    public String loginSuccess(String username, Model model) {
+        model.addAttribute("username", username);
         return "loginSuccess";
     }
 
