42fd5d122d3511c2e4b1c5d231f18daddfbd88a5
JD Reighard <jreighard9226@gmail.com>
2026-08-27 22:25:47 -0600

    fixed package name

---
+++ src/main/java/edu/carroll/cs341/Cs341DemoApplication.java
@@ -0,0 +1,13 @@
+package edu.carroll.cs341;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs341DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs341DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/carroll/cs341/Cs341DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.carroll.cs341;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs341DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
