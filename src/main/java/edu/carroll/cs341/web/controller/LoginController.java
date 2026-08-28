e97beb0f4b1fb5fd6dfc92d7f0fc8176a80b72f5
JD Reighard <jreighard9226@gmail.com>
2026-08-28 12:26:12 -0600

    added sql backend

--- src/main/java/edu/carroll/cs341/web/controller/LoginController.java
+++ src/main/java/edu/carroll/cs341/web/controller/LoginController.java
@@ -1,5 +1,6 @@
 package edu.carroll.cs341.web.controller;
 
+import edu.carroll.cs341.service.LoginService;
 import edu.carroll.cs341.web.form.LoginForm;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
@@ -13,9 +14,12 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
-    // XXX - If anything like this is used in a real application, I will hunt you down and embarrass you to all your peers.
-    private static final String validUser = "cs341user";
-    private static final String validPass = "supersecret";
+    private final LoginService loginService;
+
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
     @GetMapping("/login")
     public String loginGet(Model model) {
         model.addAttribute("loginForm", new LoginForm());
@@ -28,10 +32,7 @@ public class LoginController {
             return "login";
         }
 
-        // Username does not have to match the case, but password must be an exact match.
-        // XXX - NEVER do password validation using a hardcoded string
-        if (!(validUser.equalsIgnoreCase(loginForm.getUsername()) &&
-                validPass.equals(loginForm.getPassword()))) {
+        if (!loginService.validateUser(loginForm)) {
             result.addError(new ObjectError("globalError", "Username and password do not match known users"));
             return "login";
         }
