82c82a3855140c2a87272b96950ec3ce58e95f0d
JD Reighard <jreighard9226@gmail.com>
2026-08-31 10:26:53 -0600

    added logging to the loging service impl

--- src/main/java/edu/carroll/cs341/service/LoginServiceImpl.java
+++ src/main/java/edu/carroll/cs341/service/LoginServiceImpl.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import edu.carroll.cs341.jpa.model.Login;
 import edu.carroll.cs341.jpa.repo.LoginRepository;
-import edu.carroll.cs341.web.form.LoginForm;
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class LoginServiceImpl implements LoginService {
+    private static final Logger log = LoggerFactory.getLogger(LoginServiceImpl.class);
     private final LoginRepository loginRepo;
 
     public LoginServiceImpl(LoginRepository loginRepo) {
@@ -27,15 +29,20 @@ public class LoginServiceImpl implements LoginService {
         List<Login> users = loginRepo.findByUsernameIgnoreCase(username);
 
         // We expect 0 or 1, so if we get more than 1, bail out as this is an error we don't deal with properly.
-        if (users.size() != 1)
+        if (users.size() != 1) {
+            log.debug("validateUser: found {} users", users.size());
             return false;
+        }
         Login u = users.getFirst();
 
         final String userProvidedHash = Integer.toString(password.hashCode());
-        if (!u.getHashedPassword().equals(userProvidedHash))
+        if (!u.getHashedPassword().equals(userProvidedHash)) {
+            log.debug("validateUser: password !match");
             return false;
+        }
 
         // User exists, and the provided password matches the hashed password in the database.
+        log.info("validateUser: successful login for {}", username);
         return true;
     }
 }
